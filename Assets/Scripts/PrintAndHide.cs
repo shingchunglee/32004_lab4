7485a97deb96f8181f141245d892c015428b38e6
lee shing chung <shingchung.lee@student.uts.edu.au>
2023-08-30 16:07:26 +1000

    rename ConsolePrint to PrintAndHide and add tags to prefabs

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ConsolePrint : MonoBehaviour
 {
     private int i;
+    public Renderer rend;
+
     // Start is called before the first frame update
     void Start()
     {
