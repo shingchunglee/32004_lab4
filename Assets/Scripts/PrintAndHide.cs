1f7d97e6ff11c0b441d13174cade7c5f94895e3b
lee shing chung <shingchung.lee@student.uts.edu.au>
2023-08-30 16:27:16 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class PrintAndHide: MonoBehaviour
 {
     private int i;
+    private int randomNumber;
     public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        randomNumber = Random.Range(150, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameObject.tag == "Blue" && i == randomNumber)
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
         Debug.Log($"{gameObject.name}:{++i}");
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
